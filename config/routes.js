3570941b9e91e67fcd4b21fce90e223550a2220c
lek-tin <lek_tin@yahoo.com>
2019-03-19 23:53:11 -0700

    dev 1. created script for edge_detection. 2. changed UI views;

---
+++ config/routes.js
@@ -0,0 +1,5 @@
+var routes = {
+  EDGE_DETECTION: 'edge_detection',
+  RSA_HASHING: 'rsa_hashing'
+}
+module.exports = routes;
---
+++ functions/edge_detection.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+now=$(date +"%T") &&
+echo $now > functions/edge_detection/timestamp.txt
